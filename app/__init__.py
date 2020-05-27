daa9b3d77081d8d673501c1c7bbe7f7ebf27f5f0
Chuan Cheng <c_chuan23@hotmail.com>
2020-05-27 15:35:37 +0100

    Add file for initialising applications

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+import os
+
+from instance.config import Config
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from app.common.config import Config as AppCfg
+from app.risk.position_size import PositionSize
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
+from app.models import users
+
+# create OANDA V20 api and context
+os.environ["TESTING"] = "FALSE"
+ctx = AppCfg()
+ctx.load()
+ctx.validate()
+api = ctx.create_context()
+ps = PositionSize(ctx, api)
+random_message = "Hello :)"
