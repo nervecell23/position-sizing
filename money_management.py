2695e1ee48835a521b0f03eed63bfad625950a37
Chuan Cheng <c_chuan23@hotmail.com>
2020-07-04 18:04:45 +0100

    Trivial

--- money_management.py
+++ money_management.py
@@ -1,12 +1,12 @@
-from app import app, random_message, ps
+from app import pos_size_app, random_message, ps
 from flask import request
-
-@app.route("/")
+# a random comment
+@pos_size_app.route("/")
 def hello():
     # return "Humble Money Management Tool"
     return random_message
 
-@app.route("/position_size")
+@pos_size_app.route("/position_size")
 def position_size():
     kwargs = {}
     ticker = request.args.get("ticker")
@@ -15,16 +15,16 @@ def position_size():
     if manual_balance != "":
         kwargs = {"manual_balance": float(manual_balance)}
     ps.calculate_position_size(ticker, granularity, **kwargs)
-    return ps.output_result()
-    # return random_message
+    result = ps.output_result()
+    return result, 200
 
-@app.route("/oanda_fees")
+@pos_size_app.route("/oanda_fees")
 def oanda_fees():
     return random_message + ' oanda fees'
 
-@app.route("/etoro_fees")
+@pos_size_app.route("/etoro_fees")
 def etoro_fees():
     return random_message + ' etoro fees'
 
 if __name__ == '__main__':
-    app.run()
+    pos_size_app.run(host="0.0.0.0")
