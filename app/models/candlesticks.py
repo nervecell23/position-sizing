8675682ae78a094d80369c0abba96f06644ae231
Chuan Cheng <c_chuan23@hotmail.com>
2020-07-11 17:07:10 +0100

    Add model for candlesticks

--- app/models/candlesticks.py
+++ app/models/candlesticks.py
@@ -1,19 +1,19 @@
-from app import db 
+from app import db
 
 class Candlesticks(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     dt = db.Column(db.DateTime)
     ticker = db.Column(db.String(128))
     ask = db.Column(db.Float)
     bid = db.Column(db.Float)
 
-    def __init__(self, id, dt, ask, bid):
-        self.dt = dt 
+    def __init__(self, id, ticker, dt, ask, bid):
+        self.dt = dt
         self.ticker = ticker
-        self.ask = ask 
-        self.bid = bid 
+        self.ask = ask
+        self.bid = bid
 
     def __repr__(self):
         return f"<id {self.id}>"
-
 
