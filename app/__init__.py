aac81dcd16c5d208fbeaf003c52f13876fcb6aed
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-07-13 20:36:42 +0100

    Add configure file

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,18 @@
-import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
-from config import DevelopmentConfig
 from app.common.config import Config as APIConfig
 from app.risk.position_size import PositionSize
-from app.models import db, Candlesticks
+from app.models import db
+from config import Config as APP_DEF_CONFIG
 
 ps_app = Flask(__name__)
 CORS(ps_app)
-ps_app.config.from_object(DevelopmentConfig)
-ps_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ps_app.config.from_object(APP_DEF_CONFIG)
 db.init_app(ps_app)
 migrate = Migrate(ps_app, db)
 
 # create OANDA V20 api and context
-os.environ["TESTING"] = "TRUE"
 ctx = APIConfig()
 ctx.load()
 ctx.validate()
