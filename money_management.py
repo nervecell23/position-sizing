1c4a2482ecdfa13a7dbf02e0832b940747b04f66
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-07-22 11:25:33 +0100

    WIP

--- money_management.py
+++ money_management.py
@@ -1,4 +1,5 @@
 from app import ps_app, ps
+from werkzeug.exceptions import BadRequest
 from flask import request
 # a random comment
 @ps_app.route("/")
@@ -12,21 +13,26 @@ def position_size():
     ticker = request.args.get("ticker")
     granularity = request.args.get("granularity")
     manual_balance_gbp = request.args.get("manual_balance_gbp")
+    manual_balance_usd = request.args.get('manual_balance_usd')
     if manual_balance_gbp != "":
-        kwargs = {"manual_balance": float(manual_balance)}
+        kwargs = {"manual_balance": float(manual_balance_gbp)}
     if manual_balance_usd == '':
         #raise error message
+        raise BadRequest('Balance of USD account cannot be None')
     ps.calculate_position_size(ticker, granularity, **kwargs)
     result = ps.output_result()
     return result, 200
 
+@ps_app.route('/test')
+def test_func():
+    raise BadRequest('This is a test')
+    return 'This should not be reached'
+
 @ps_app.route("/oanda_fees")
 def oanda_fees():
-    return random_message + ' oanda fees'
-
+    pass
 @ps_app.route("/etoro_fees")
 def etoro_fees():
-    return random_message + ' etoro fees'
-
+    pass
 if __name__ == '__main__':
     ps_app.run()
