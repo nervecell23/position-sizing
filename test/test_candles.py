73a4c1452665643ebcad4d96990332113d2a0bd6
Chuan Cheng <chuancheng@Chuans-MacBook-Pro-2.local>
2020-08-17 18:01:11 +0100

    WIP add test for Candles class

--- test/test_candles.py
+++ test/test_candles.py
@@ -1,36 +1,9 @@
 import pytest
-from app.risk.config import Config
+from app.common.config import Config
 from app.risk.candles import Candles
 from datetime import datetime
+from unittest.mock import patch, Mock, MagicMock 
 
-@pytest.fixture
-def api():
-    cfg = Config()
-    cfg.load()
-    return cfg.create_context()
-
-@pytest.fixture
-def subject(api):
-    subject = Candles(api)
-    return subject
-
-def test_fetch_candles(subject):
-    time, result = subject.fetch_candles(15)
-    assert len(result) == 15
-    for element in result:
-        assert type(element.o) is float
-        assert type(element.h) is float
-        assert type(element.l) is float
-        assert type(element.c) is float
-
-    assert type(time) is datetime
-
-    time, result = subject.fetch_candles(1)
-    assert len(result) == 1
-    for element in result:
-        assert type(element.o) is float
-        assert type(element.h) is float
-        assert type(element.l) is float
-        assert type(element.c) is float
-
-    assert type(time) is datetime
+@path('')
+class TestCandles:
+    def 
